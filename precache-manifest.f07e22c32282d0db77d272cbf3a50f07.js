1bef9db46a56ca27833e9cac77154523c88b0c8d
sarah <sarachamorro@gmail.com>
2020-04-14 19:07:00 -0300

    Updates

---
+++ precache-manifest.f07e22c32282d0db77d272cbf3a50f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923e6195abf041e0a7889429a94d37c5",
+    "url": "/ao-kanji/index.html"
+  },
+  {
+    "revision": "a9b5d2d7b35d3241e36e",
+    "url": "/ao-kanji/static/css/main.94aa86d5.chunk.css"
+  },
+  {
+    "revision": "fc6cf54472af123d2fba",
+    "url": "/ao-kanji/static/js/2.f98dc233.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ao-kanji/static/js/2.f98dc233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b5d2d7b35d3241e36e",
+    "url": "/ao-kanji/static/js/main.e1709bff.chunk.js"
+  },
+  {
+    "revision": "5c20258e1d4a25314224",
+    "url": "/ao-kanji/static/js/runtime-main.79f02abc.js"
+  }
+]);
